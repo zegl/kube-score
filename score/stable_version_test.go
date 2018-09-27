7795700c5d87eae8e437c000e2bc8454a49a9353
Gustav Westling <hello@gustav.tv>
2018-09-27 20:57:31 +0200

    score: rename 'Unstable version' to 'Stable version'

---
+++ score/stable_version_test.go
@@ -0,0 +1,44 @@
+package score
+
+import "testing"
+
+
+func TestStatefulSetAppsv1beta1(t *testing.T) {
+	testExpectedScore(t, "statefulset-appsv1beta1.yaml", "Stable version", 5)
+}
+
+func TestStatefulSetAppsv1beta2(t *testing.T) {
+	testExpectedScore(t, "statefulset-appsv1beta2.yaml", "Stable version", 5)
+}
+
+func TestDeploymentExtensionsv1beta1(t *testing.T) {
+	testExpectedScore(t, "deployment-extensions-v1beta1.yaml", "Stable version", 5)
+}
+
+func TestDeploymentAppsv1beta1(t *testing.T) {
+	testExpectedScore(t, "deployment-appsv1beta1.yaml", "Stable version", 5)
+}
+
+func TestDeploymentAppsv1beta2(t *testing.T) {
+	testExpectedScore(t, "deployment-appsv1beta2.yaml", "Stable version", 5)
+}
+
+func TestDaemonSetAppsv1(t *testing.T) {
+	testExpectedScore(t, "daemonset-appsv1.yaml", "Stable version", 10)
+}
+
+func TestDaemonSetAppsv1beta2(t *testing.T) {
+	testExpectedScore(t, "daemonset-appsv1beta2.yaml", "Stable version", 5)
+}
+
+func TestDaemonSetExtensionsv1beta1(t *testing.T) {
+	testExpectedScore(t, "daemonset-extensionsv1beta1.yaml", "Stable version", 5)
+}
+
+func TestCronJobBatchv1beta1(t *testing.T) {
+	testExpectedScore(t, "cronjob-batchv1beta1.yaml", "Stable version", 10)
+}
+
+func TestJobBatchv1(t *testing.T) {
+	testExpectedScore(t, "job-batchv1.yaml", "Stable version", 10)
+}
