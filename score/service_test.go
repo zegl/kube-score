7f757c07b9535fc4ad90ab82051d101e7853a041
Gustav Westling <hello@gustav.tv>
2018-10-09 20:46:17 +0200

    score: add Service Targets Pod check
    Fixes #13

---
+++ score/service_test.go
@@ -0,0 +1,27 @@
+package score
+
+import "testing"
+
+func TestServiceTargetsPodDeployment(t *testing.T) {
+	testExpectedScore(t, "service-target-deployment.yaml", "Service Targets Pod", 10)
+}
+
+func TestServiceNotTargetsPodDeployment(t *testing.T) {
+	testExpectedScore(t, "service-not-target-deployment.yaml", "Service Targets Pod", 0)
+}
+
+func TestServiceTargetsPodRaw(t *testing.T) {
+	testExpectedScore(t, "service-target-pod.yaml", "Service Targets Pod", 10)
+}
+
+func TestServiceNotTargetsPodRaw(t *testing.T) {
+	testExpectedScore(t, "service-not-target-pod.yaml", "Service Targets Pod", 0)
+}
+
+func TestServiceTargetsPodRawMultiLabel(t *testing.T) {
+	testExpectedScore(t, "service-target-pod-multi-label.yaml", "Service Targets Pod", 10)
+}
+
+func TestServiceNotTargetsPodRawMultiLabel(t *testing.T) {
+	testExpectedScore(t, "service-not-target-pod-multi-label.yaml", "Service Targets Pod", 0)
+}
