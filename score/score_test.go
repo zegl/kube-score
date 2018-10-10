ee4d01a4375086194118a304de2e60bbc4aa5809
Gustav Westling <hello@gustav.tv>
2018-10-10 22:17:02 +0200

    probes: namespace support in pod probe targeted by service detection

--- score/score_test.go
+++ score/score_test.go
@@ -134,6 +134,20 @@ func TestProbesTargetedByService(t *testing.T) {
 	testExpectedScore(t, "pod-probes-targeted-by-service.yaml", "Pod Probes", 0)
 }
 
+func TestProbesTargetedByServiceSameNamespace(t *testing.T) {
+	comments := testGetComments(t, "pod-probes-targeted-by-service-same-namespace.yaml", "Pod Probes")
+	assert.Len(t, comments, 1)
+	assert.Equal(t, "Container is missing a readinessProbe", comments[0].Summary)
+
+	testExpectedScore(t, "pod-probes-targeted-by-service-same-namespace.yaml", "Pod Probes", 0)
+}
+
+func TestProbesTargetedByServiceDifferentNamespace(t *testing.T) {
+	comments := testGetComments(t, "pod-probes-targeted-by-service-different-namespace.yaml", "Pod Probes")
+	assert.Len(t, comments, 0)
+	testExpectedScore(t, "pod-probes-targeted-by-service-different-namespace.yaml", "Pod Probes", 10)
+}
+
 func TestProbesTargetedByServiceNotTargeted(t *testing.T) {
 	testExpectedScore(t, "pod-probes-not-targeted-by-service.yaml", "Pod Probes", 10)
 }
