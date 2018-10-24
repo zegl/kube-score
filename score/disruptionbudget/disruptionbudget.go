a823278378f003b2cb1a92a10dff48699c610f4b
Gustav Westling <hello@gustav.tv>
2018-10-24 20:53:36 +0200

    disruptionbudget: make sure that the namespace matches

--- score/disruptionbudget/disruptionbudget.go
+++ score/disruptionbudget/disruptionbudget.go
@@ -8,8 +8,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func hasMatching(budgets []policyv1beta1.PodDisruptionBudget, lables map[string]string) bool {
+func hasMatching(budgets []policyv1beta1.PodDisruptionBudget, namespace string, lables map[string]string) bool {
 	for _, budget := range budgets {
+		if budget.Namespace != namespace {
+			continue
+		}
+
 		selector, err := metav1.LabelSelectorAsSelector(budget.Spec.Selector)
 		if err != nil {
 			panic(err)
@@ -28,7 +32,7 @@ func ScoreStatefulSetHas(budgets []policyv1beta1.PodDisruptionBudget) func(appsv
 		score.Name = "StatefulSet has PodDisruptionBudget"
 		score.ID = "statefulset-has-poddisruptionbudget"
 
-		if hasMatching(budgets, statefulset.Spec.Template.Labels) {
+		if hasMatching(budgets, statefulset.Namespace, statefulset.Spec.Template.Labels) {
 			score.Grade = scorecard.GradeAllOK
 		} else {
 			score.Grade = scorecard.GradeCritical
@@ -44,7 +48,7 @@ func ScoreDeploymentHas(budgets []policyv1beta1.PodDisruptionBudget) func(appsv1
 		score.Name = "Deployment has PodDisruptionBudget"
 		score.ID = "deployment-has-poddisruptionbudget"
 
-		if hasMatching(budgets, deployment.Spec.Template.Labels) {
+		if hasMatching(budgets, deployment.Namespace, deployment.Spec.Template.Labels) {
 			score.Grade = scorecard.GradeAllOK
 		} else {
 			score.Grade = scorecard.GradeCritical
