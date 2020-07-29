0a631afd059bb90ab6cb3d772a38b2840d558e61
Gustav Westling <gustav@westling.dev>
2020-07-29 13:18:26 +0200

    hack/krew: make compatible with new version of goreleaser

--- hack/krew-index.sh
+++ hack/krew-index.sh
@@ -6,17 +6,20 @@ set -x
 VERSION=$(git describe --tags --abbrev=0 | cut -c2-)
 KREW_INDEX_PATH="${HOME}/src/krew-index"
 FILE="${KREW_INDEX_PATH}/plugins/score.yaml"
+KUBE_SCORE_REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 checksum() {
-    grep -E "kube-score_${VERSION}_${1}_amd64.tar.gz" dist/checksums.txt | awk '{print $1}'
+    grep -E "kube-score_${VERSION}_${1}_amd64.tar.gz" ${KUBE_SCORE_REPO_ROOT}/cmd/kube-score/dist/kube-score_${VERSION}_checksums.txt | awk '{print $1}'
 }
 
 gg() {
     git -C "$KREW_INDEX_PATH" "$@"
 }
 
+gg checkout master
 gg fetch origin
 gg reset --hard origin/master
+gg branch -D "kube-score-${VERSION}"
 gg checkout -b "kube-score-${VERSION}"
 
 yq write --inplace "$FILE" "spec.version" "v${VERSION}"
@@ -29,6 +32,6 @@ yq write --inplace "$FILE" "spec.platforms[1].sha256" "$(checksum linux)"
 
 gg add plugins/score.yaml
 gg commit -m "Update score to v${VERSION}"
-gg push -u zegl "kube-score-${VERSION}"
+gg push --force-with-lease -u zegl "kube-score-${VERSION}"
 
 open "https://github.com/kubernetes-sigs/krew-index/compare/master...zegl:kube-score-${VERSION}?expand=1"
