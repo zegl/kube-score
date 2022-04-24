da33cf583f623ca1362909e45de4deea50c11b4c
Mads Jensen <atombrella@users.noreply.github.com>
2022-04-24 17:31:12 +0200

    Fix lint

--- cmd/kube-score/cmd_version.go
+++ cmd/kube-score/cmd_version.go
@@ -8,7 +8,6 @@ import (
 var version = "development"
 var commit = "N/A"
 var date = "N/A"
-var uri = "https://kube-score.com/"
 
 func cmdVersion() {
 	fmt.Printf("kube-score version: %s, commit: %s, built: %s\n", version, commit, date)
