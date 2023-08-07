dbc844c5f8301fec1fcced5dc5b6063edcbec54a
Gustav Westling <gustav@polar.sh>
2023-08-07 13:38:05 +0200

    score/disruptionbudget: improved error message for PDB found but in wrong namespace

--- score/poddisruptionbudget_test.go
+++ score/poddisruptionbudget_test.go
@@ -3,6 +3,8 @@ package score
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 	"github.com/zegl/kube-score/scorecard"
 )
 
@@ -63,5 +65,32 @@ func TestDeploymentPodDisruptionBudgetV1Matches(t *testing.T) {
 
 func TestDeploymentPodDisruptionBudgetV1NoMatch(t *testing.T) {
 	t.Parallel()
-	testExpectedScore(t, "deployment-poddisruptionbudget-v1-no-match.yaml", "Deployment has PodDisruptionBudget", scorecard.GradeCritical)
+	actual := testExpectedScore(t, "deployment-poddisruptionbudget-v1-no-match.yaml", "Deployment has PodDisruptionBudget", scorecard.GradeCritical)
+
+	expected := []scorecard.TestScoreComment{
+		{
+			Path:        "",
+			Summary:     "No matching PodDisruptionBudget was found",
+			Description: "It's recommended to define a PodDisruptionBudget to avoid unexpected downtime during Kubernetes maintenance operations, such as when draining a node. ",
+		},
+	}
+
+	diff := cmp.Diff(expected, actual)
+	assert.Empty(t, diff)
+}
+
+func TestDeploymentPodDisruptionBudgetV1NoMatchMatchInOtherNamespace(t *testing.T) {
+	t.Parallel()
+	actual := testExpectedScore(t, "deployment-poddisruptionbudget-v1-different-namespace.yaml", "Deployment has PodDisruptionBudget", scorecard.GradeCritical)
+
+	expected := []scorecard.TestScoreComment{
+		{
+			Path:        "",
+			Summary:     "No matching PodDisruptionBudget was found",
+			Description: "It's recommended to define a PodDisruptionBudget to avoid unexpected downtime during Kubernetes maintenance operations, such as when draining a node. A matching budget was found, but in a different namespace. expected='foo' got='[not-foo bar]'",
+		},
+	}
+
+	diff := cmp.Diff(expected, actual)
+	assert.Empty(t, diff)
 }
