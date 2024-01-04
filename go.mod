32ce7ab3b4da8b37984a79ce3c42a713b5cb9a1e
BigGold1310 <cyrill.naef@gmail.com>
2024-01-04 13:05:45 +0100

    score/deployment: Adding check for the Deployment replicas to be >=2 if not HPA managed and targeted by Service
    Fixes #569
    
    Signed-off-by: BigGold1310 <cyrill.naef@gmail.com>

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.29.0
 	k8s.io/apimachinery v0.29.0
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b
 )
 
 require (
@@ -29,7 +30,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
-	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
--- score/deployment/deployment.go
+++ score/deployment/deployment.go
@@ -6,10 +6,13 @@ import (
 	"github.com/zegl/kube-score/score/internal"
 	"github.com/zegl/kube-score/scorecard"
 	v1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	"k8s.io/utils/ptr"
 )
 
 func Register(allChecks *checks.Checks, all ks.AllTypes) {
-	allChecks.RegisterDeploymentCheck("Deployment Strategy", `Makes sure that all Deploymtes targeted by service use RollingUpdate strategy`, deploymentRolloutStrategy(all.Services()))
+	allChecks.RegisterDeploymentCheck("Deployment Strategy", `Makes sure that all Deployments targeted by service use RollingUpdate strategy`, deploymentRolloutStrategy(all.Services()))
+	allChecks.RegisterDeploymentCheck("Deployment Replicas", `Makes sure that Deployment has multiple replicas`, deploymentReplicas(all.Services(), all.HorizontalPodAutoscalers()))
 }
 
 // deploymentRolloutStrategy checks if a Deployment has the update strategy on RollingUpdate if targeted by a service
@@ -33,15 +36,71 @@ func deploymentRolloutStrategy(svcs []ks.Service) func(deployment v1.Deployment)
 		}
 
 		if referencedByService {
-			if deployment.Spec.Strategy.Type == v1.RollingUpdateDeploymentStrategyType {
+			if deployment.Spec.Strategy.Type == v1.RollingUpdateDeploymentStrategyType || deployment.Spec.Strategy.Type == "" {
 				score.Grade = scorecard.GradeAllOK
 			} else {
 				score.Grade = scorecard.GradeWarning
-				score.AddCommentWithURL("", "Deployment update strategy", "The deployment is used by a service but not using rolling update strategy which can cause interruptions", "https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#strategy")
+				score.AddCommentWithURL("", "Deployment update strategy", "The deployment is used by a service but not using the RollingUpdate strategy which can cause interruptions. Set .spec.strategy.type to RollingUpdate.", "https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#strategy")
 			}
 		} else {
 			score.Skipped = true
-			score.AddComment("", "Skipped as the deployment strategy does not matter if not targeted by a service", "")
+			score.AddComment("", "Skipped as the Deployment is not targeted by a service", "")
+		}
+
+		return
+	}
+}
+
+// deploymentReplicas checks if a Deployment has >= 2 replicas if not (targeted by service || has HorizontalPodAutoscaler)
+func deploymentReplicas(svcs []ks.Service, hpas []ks.HpaTargeter) func(deployment v1.Deployment) (scorecard.TestScore, error) {
+	svcsInNamespace := make(map[string][]map[string]string)
+	for _, s := range svcs {
+		svc := s.Service()
+		if _, ok := svcsInNamespace[svc.Namespace]; !ok {
+			svcsInNamespace[svc.Namespace] = []map[string]string{}
+		}
+		svcsInNamespace[svc.Namespace] = append(svcsInNamespace[svc.Namespace], svc.Spec.Selector)
+	}
+	hpasInNamespace := make(map[string][]autoscalingv1.CrossVersionObjectReference)
+	for _, hpa := range hpas {
+		hpaTarget := hpa.HpaTarget()
+		hpaMeta := hpa.GetObjectMeta()
+		if _, ok := hpasInNamespace[hpaMeta.Namespace]; !ok {
+			hpasInNamespace[hpaMeta.Namespace] = []autoscalingv1.CrossVersionObjectReference{}
+		}
+		hpasInNamespace[hpaMeta.Namespace] = append(hpasInNamespace[hpaMeta.Namespace], hpaTarget)
+	}
+
+	return func(deployment v1.Deployment) (score scorecard.TestScore, err error) {
+		referencedByService := false
+		hasHPA := false
+
+		for _, svcSelector := range svcsInNamespace[deployment.Namespace] {
+			if internal.LabelSelectorMatchesLabels(svcSelector, deployment.Spec.Template.Labels) {
+				referencedByService = true
+				break
+			}
+		}
+
+		for _, hpaTarget := range hpasInNamespace[deployment.Namespace] {
+			if deployment.TypeMeta.APIVersion == hpaTarget.APIVersion &&
+				deployment.TypeMeta.Kind == hpaTarget.Kind &&
+				deployment.ObjectMeta.Name == hpaTarget.Name {
+				hasHPA = true
+				break
+			}
+		}
+
+		if !referencedByService || hasHPA {
+			score.Skipped = true
+			score.AddComment("", "Skipped as the Deployment is not targeted by service or is controlled by a HorizontalPodAutoscaler", "")
+		} else {
+			if ptr.Deref(deployment.Spec.Replicas, 1) >= 2 {
+				score.Grade = scorecard.GradeAllOK
+			} else {
+				score.Grade = scorecard.GradeWarning
+				score.AddComment("", "Deployment few replicas", "Deployments targeted by Services are recommended to have at least 2 replicas to prevent unwanted downtime.")
+			}
 		}
 
 		return
